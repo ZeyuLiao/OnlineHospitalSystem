7a82ee78d4a0939d94d50b5a99a7601f75e905bd
Leia <18764436236@163.com>
2022-10-21 00:19:41 -0400

    Sql

---
+++ src/sql/Test add data.sql
@@ -0,0 +1,8 @@
+INSERT INTO login ( NAME, PASSWORD, State )
+VALUES
+	( "Ruolin", "123", 1 ),
+	( "ZeYu", "123", 2 ),
+	( "Ziyu", "123", 5 ),
+	( "Admin1", "123", 3 ),
+	( "Admin2", "123", 4 ),
+	( "Admin3", "123", 5 );
